f63381dcc4e5ac2e93bda1e49ff6a51c913784c1
amale <aidrissi001@gmail.com>
2021-10-21 11:23:21 +0200

    add of dto classes in User Microservice

---
+++ microservice-user/src/main/java/com/muser/dto/LocationDto.java
@@ -0,0 +1,14 @@
+package com.muser.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationDto {
+    private double latitude;
+    private double longitude;
+}
+
