cab8d4bcef69436217d77f51d41105c4030d15c6
amale <aidrissi001@gmail.com>
2021-11-16 20:39:39 +0100

    update of docker-compose file

--- microservice-rewards/src/main/java/com/mrewards/model/UserReward.java
+++ microservice-rewards/src/main/java/com/mrewards/model/UserReward.java
@@ -3,12 +3,14 @@ package com.mrewards.model;
 import com.mrewards.dto.AttractionDto;
 import com.mrewards.dto.VisitedLocationDto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 /**
  * A model class which gathers user's data about his reward points and the attraction he visited.
  */
 @Getter
+@NoArgsConstructor
 @ToString
 public class UserReward {
     private VisitedLocationDto visitedLocation;
