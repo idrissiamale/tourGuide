162f6a9c42bc54077a8dca4d162c0fb0957b3cf3
amale <aidrissi001@gmail.com>
2021-11-18 09:50:13 +0100

    refactoring of UserReward model class of TripPricer microservice

--- microservice-tripPricer/src/main/java/com/mtrippricer/model/UserReward.java
+++ microservice-tripPricer/src/main/java/com/mtrippricer/model/UserReward.java
@@ -1,19 +1,15 @@
 package com.mtrippricer.model;
 
-import com.mtrippricer.dto.AttractionDto;
-import com.mtrippricer.dto.VisitedLocationDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 /**
- * A model class which gathers user's data about his reward points and the attraction he visited.
+ * A model class which gathers user's data about his reward points.
  */
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserReward {
-	private VisitedLocationDto visitedLocation;
-	private AttractionDto attraction;
-	private int rewardPoints;
+    private int rewardPoints;
 }
